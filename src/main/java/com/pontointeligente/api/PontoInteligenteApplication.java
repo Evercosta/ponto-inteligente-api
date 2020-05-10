2b47d28a11232ed94b908478ef077a049163c0c6
Everton Costa <everton@customtrade.com.br>
2020-05-10 01:40:09 -0300

    Add Auth com token JWT

--- src/main/java/com/pontointeligente/api/PontoInteligenteApplication.java
+++ src/main/java/com/pontointeligente/api/PontoInteligenteApplication.java
@@ -3,7 +3,7 @@ package com.pontointeligente.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication()
 public class PontoInteligenteApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/pontointeligente/api/security/config/WebSecurityConfig.java
+++ src/main/java/com/pontointeligente/api/security/config/WebSecurityConfig.java
@@ -3,6 +3,8 @@ package com.pontointeligente.api.security.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.BeanIds;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -12,10 +14,10 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-// import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import com.pontointeligente.api.security.JwtAuthenticationEntryPoint;
-// import com.pontointeligente.api.security.filters.JwtAuthenticationTokenFilter;
+import com.pontointeligente.api.security.filters.JwtAuthenticationTokenFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -38,32 +40,26 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 		return new BCryptPasswordEncoder();
 	}
 
-	// @Bean
-	// public JwtAuthenticationTokenFilter authenticationTokenFilterBean() throws Exception {
-	// 	return new JwtAuthenticationTokenFilter();
-	// }
+	@Bean
+	public JwtAuthenticationTokenFilter authenticationTokenFilterBean() throws Exception {
+		return new JwtAuthenticationTokenFilter();
+	}
 
-	// @Override
-	// protected void configure(HttpSecurity httpSecurity) throws Exception {
-	// 	httpSecurity.csrf().disable().exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
-	// 			.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and().authorizeRequests()
-	// 			.antMatchers("/auth/**", "/api/cadastrar-pj", "/api/cadastrar-pf", "/v2/api-docs",
-	// 					"/swagger-resources/**", "/configuration/security", "/swagger-ui.html", "/webjars/**")
-	// 			.permitAll().anyRequest().authenticated();
-	// 	httpSecurity.addFilterBefore(authenticationTokenFilterBean(), UsernamePasswordAuthenticationFilter.class);
-	// 	httpSecurity.headers().cacheControl();
-    // }
-    
-    // configure para teste
-    @Override
-    protected void configure(HttpSecurity httpSecurity) throws Exception {
-        httpSecurity.csrf().disable().exceptionHandling()
-        .authenticationEntryPoint(unauthorizedHandler)
-        .and().sessionManagement()
-        .sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
-        .authorizeRequests()
-        .antMatchers("/auth/**", "/api/cadastrar-pj", "/api/cadastrar-pf", "/v2/api-docs", "/swagger-resources/**", "/configuration/security", "/swagger-ui.html", "/webjars/**")
-        .permitAll().anyRequest().authenticated();		
-    }
+	@Bean(name = BeanIds.AUTHENTICATION_MANAGER)
+	@Override
+		public AuthenticationManager authenticationManagerBean() throws Exception {
+		return super.authenticationManagerBean();
+	}
+
+	@Override
+	protected void configure(HttpSecurity httpSecurity) throws Exception {
+		httpSecurity.csrf().disable().exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
+				.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and().authorizeRequests()
+				.antMatchers("/auth/**", "/api/cadastrar-pj", "/api/cadastrar-pf", "/v2/api-docs",
+						"/swagger-resources/**", "/configuration/security", "/swagger-ui.html", "/webjars/**")
+				.permitAll().anyRequest().authenticated();
+		httpSecurity.addFilterBefore(authenticationTokenFilterBean(), UsernamePasswordAuthenticationFilter.class);
+		httpSecurity.headers().cacheControl();
+	}
 
 }
